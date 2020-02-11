e658bc58507a8b5a6798377fc162c0e0e5261606
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2020-02-10 23:50:40 -0800

    feb 10 - 11:50pm

--- game.py
+++ game.py
@@ -1,1 +1,20 @@
+import turtle # library for games
 
+win = turtle.Screen() # create window 
+win.title("Pong Game")
+win.bgcolor("black")
+win.setup(width=800, height = 600)
+win.tracer(0)
+
+#paddle A
+paddle_a = turtle.Turtle()
+
+
+#paddle B 
+paddle_b= turtle.Turtle()
+
+# Ball 
+
+# main game loop 
+while True: 
+    win.update()
